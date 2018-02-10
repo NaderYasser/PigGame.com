bd53118cef756447f2fcfce9445b4df8297b2a1b
Nader Yasser <naderdd7@gmail.com>
2018-02-10 14:02:06 +0200

    button roll eventListener

--- app.js
+++ app.js
@@ -8,10 +8,42 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores , roundScore,activePlayer,dice;
+var scores , roundScore,activePlayer;
 score = [0,0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random()*6)+1 ;
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
 document.querySelector('.dice').style.display='none';
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+document.querySelector('.btn-roll').addEventListener('click',function () {
+  //random number
+  var dice = Math.floor(Math.random()*6)+1 ;
+
+  //display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display='block';
+  diceDOM.src = 'dice-'+dice+'.png';
+
+  //update the score if the rolled number is not 1
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-'+activePlayer).textContent= roundScore;
+  }
+  else {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0 ;
+    roundScore = 0;
+    document.getElementById('current-0').textContent='0';
+    document.getElementById('current-1').textContent='0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display='none';
+
+
+  }
+
+});
